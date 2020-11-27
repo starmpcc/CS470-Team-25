5d647ab556dc228bf2732cfa58a3a617bb44486f
springcat <grace12021@gmail.com>
2020-11-28 01:11:37 +0900

    Change names

--- cat_id/flask_deep/__init__.py
+++ cat_id/flask_deep/__init__.py
@@ -2,32 +2,41 @@ import os
 import sys
 from flask import *
 import torch
+
+
+
 app = Flask(__name__)
 app.debug = True
 
 root = os.getcwd()
+<<<<<<< Updated upstream
 from Classifier import CatFaceIdentifier, val_transform
+=======
+
+from Classifier import CatFaceIdentifier, temp_transform
+>>>>>>> Stashed changes
 
 model = CatFaceIdentifier().cuda()
 checkpoint = torch.load(os.path.join(root, '..', "ckpt.pt"))
 model.load_state_dict(checkpoint['model_state_dict'])
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('upload.html')
 
 
-@app.route('/identifier_get')
-def identifier_get():
-    return render_template('identifier_get.html')
+@app.route('/image_get')
+def image_get():
+    return render_template('upload.html')
 
-@app.route('/identifier_post', methods = ['GET', 'POST'])
-def identifier_post():
+
+@app.route('/result_post', methods = ['GET', 'POST'])
+def result_post():
     if request.method == 'POST':
-        user_inp = request.files['user_img']
-        img = temp_transform(user_inp)
+        user_img = request.files['user_img']
+        user_img.save("./flask_deep/images/"+str(user_img.filename))
         #Need to add Preprocess output
+        img = temp_transform(user_img)
         #Suggestion: Get Video -> Can make the accs better
         pred = model(img)
-        return render_template('identifier_post.html', pred = pred)
+    return render_template('result.html', pred = pred)
