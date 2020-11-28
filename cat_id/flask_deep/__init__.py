89384a35092caa101ed879c008cb70880f261fa5
Junu Kim <kjune0322@naver.com>
2020-11-28 21:06:32 +0900

    Connect Model to server

--- cat_id/flask_deep/__init__.py
+++ cat_id/flask_deep/__init__.py
@@ -4,7 +4,7 @@ from flask import *
 import torch
 from flask import Flask, request
 from werkzeug.utils import secure_filename
-
+from PIL import Image
 root = os.getcwd()
 
 app = Flask(__name__)
@@ -18,9 +18,9 @@ Import classifier
 TODO : Activate the comments
 TODO : In Classifier.py, val_transform must be const.
 '''
-#model = CatFaceIdentifier().cuda()
-#checkpoint = torch.load(root+"/flask_deep/ckpt.pt")
-#model.load_state_dict(checkpoint['model_state_dict'])
+model = CatFaceIdentifier().cuda()
+checkpoint = torch.load(root+"/flask_deep/ckpt.pt")
+model.load_state_dict(checkpoint['model_state_dict'])
 
 user_img_src = "images/cat.jpg"
 
@@ -43,6 +43,9 @@ def result_post():
         TODO : Activate the comments
         '''
         user_img_src = 'images/{}'.format(secure_filename(user_img.filename))
-        #img = val_transform(user_img)
-        #pred = model(img)
-    return render_template('result.html', user_img=user_img_src, pred = pred)
+        img = Image.open(os.path.join(root, "flask_deep", "static", user_img_src))
+        img = val_transform(img).unsqueeze(0).to('cuda')
+        res = model(img)
+        _, pred = torch.max(res,1)
+        _, top5 = torch.topk(res, 5, 1)
+    return render_template('result.html', user_img=user_img_src, pred = pred, top5 = top5)
